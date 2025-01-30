8799c14ece1df8fffeb49769f844daf79e61ec5e
savioferraz <savio.ferraz88@gmail.com>
2025-01-30 00:14:00 -0300

    feat: cria uma interface gráfica para envio da planilha

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
 import express from "express";
 import cors from "cors";
 import tagRouter from "./routes/tagsRoutes";
+import path from "path";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(cors()).use(express.json());
 
+// app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/tags", tagRouter);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.use(errorMiddleware);
 
